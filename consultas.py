e559eeee8d93d7bd8dde5f50fa599566c373a2b4
MicaelaNatali <micaelanatali09@gmail.com>
2026-06-10 18:53:28 -0300

    Update consultas.py
    Se agregan funciones de actualización y ordenamiento.

--- consultas.py
+++ consultas.py
@@ -29,3 +29,45 @@ def filtrar_superficie(paises):
     for pais in paises:
         if minimo <= pais ["superficie"] <=maximo:
             print(pais)
+
+def actualizar_pais(paises):
+    nombre= input("Ingrese el nombre del país a actualizar:"). lower()
+    for pais in paises:
+        if pais["nombre"].lower() == nombre:
+            pais["poblacion"] = int(input("Nueva población:"))
+            pais["superficie"] = int(input("Nueva superficie:")
+            print("País actualizado correctamente.")
+            return
+    print("País no encontrado.")
+
+def ordenar_nombre(paises):
+    for i in range(len(paises)):
+        for j in range (i+ 1, len (paises)):
+            if paises [i]["nombre"] > paises [j]["nombre"]:
+                aux = paises [i]
+                paises [i] = paises [j]
+                paises [j] = aux
+        for pais in paises:
+            print(pais)
+
+def ordenar_poblacion(paises):
+    for i in range(len(paises)):
+        for j in range (i + 1, len (paises)):
+            if paises[i]["poblacion"] > paises [j]["poblacion"]:
+                aux = paises [i]
+                paises [i] = paises [j]
+                paises [j] = aux
+    for pais in paises:
+        print (pais)
+
+def ordenar_superficie(paises):
+    for i in range (len(paises)):
+        for j in range(i + 1, len (paises)):
+            if paises [i]["superficie"] > paises [j]["superficie"]:
+                aux = paises [i]
+                paises [i] = paises [j]
+                paises [j] = aux
+    for pais in paises:
+        print (pais)
+            
+            

a2449289c4245acb6be46ed627721d38fa9c8a90
MicaelaNatali <micaelanatali09@gmail.com>
2026-06-09 21:23:33 -0300

    Update main.py
    Implementación del menú principal

---
+++ main.py
@@ -0,0 +1,54 @@
+from datos import cargar_paises, agregar_pais
+from consultas import (buscar_pais, filtrar_continente, filtrar_poblacion, filtrar_superficie)
+from estadisticas import mostrar_estadisticas
+
+
+def mostrar_menu():
+    print("\n========== GESTIÓN DE PAÍSES ==========")
+    print("1. Agregar país")
+    print("2. Buscar país")
+    print("3. Filtrar por continente")
+    print("4. Filtrar por población")
+    print("5. Filtrar por superficie")
+    print("6. Mostrar estadísticas")
+    print("7. Salir")
+    print("=======================================")
+
+
+def main():
+
+    paises = cargar_paises()
+
+    while True:
+
+        mostrar_menu()
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            agregar_pais(paises)
+
+        elif opcion == "2":
+            buscar_pais(paises)
+
+        elif opcion == "3":
+            filtrar_continente(paises)
+
+        elif opcion == "4":
+            filtrar_poblacion(paises)
+
+        elif opcion == "5":
+            filtrar_superficie(paises)
+
+        elif opcion == "6":
+            mostrar_estadisticas(paises)
+
+        elif opcion == "7":
+            print("Programa finalizado.")
+            break
+
+        else:
+            print("Opción inválida. Intente nuevamente.")
+
+
+main()
